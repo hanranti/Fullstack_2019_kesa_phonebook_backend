4da654169afef90d558f365e57d1b2537c125bd7
hanranti <hanranti@cs.helsinki.fi>
2019-07-05 10:42:17 +0300

    tehtava 3.8 tehty

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
+let morgan = require('morgan')
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
